e928d6fd0242f4a96bd2cc8fbdf63e621e9a9cca
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-01-31 23:56:16 -0600

    begin writting unit test

---
+++ src/test/java/model/PlayerTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+    @Test
+    void playerRandomName() {
+        Player randName = new Player();
+        
+    }
+
+}
