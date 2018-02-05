68ff639cd283367e026de500e650e24c392aec2f
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-02-05 06:31:42 -0600

    Begin writing test

--- src/test/java/model/piece/QueenTest.java
+++ src/test/java/model/piece/QueenTest.java
@@ -5,6 +5,8 @@ import model.Player;
 import model.Position;
 
 import java.util.Set;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,18 +19,36 @@ class QueenTest {
         Piece queen = new Queen(player);
         board.addPiece(queen, 3, 5);
 
-        Set<Position> positions = queen.getAvailablePosition(true);
-        assertTrue(positions.isEmpty());
-
-        positions = queen.getAvailablePosition(false);
+        Set<Position> positions = queen.getAvailablePosition(false);
         // check whether position is the same as expected
-        for (int row = 0; row < 8; ++row) {
-            if (row != 3)
-                assertTrue(positions.contains(new Position(row, 5)));
-            else
-                assertFalse(positions.contains(new Position(row, 5)));
-        }
+        // check rank and tile
+        assertTrue(positions.contains(new Position(2, 5)));
+        assertTrue(positions.contains(new Position(6, 5)));
+        assertTrue(positions.contains(new Position(3, 3)));
+        assertTrue(positions.contains(new Position(3, 7)));
+        // check diagonals
+        assertTrue(positions.contains(new Position(2, 4)));
+        assertTrue(positions.contains(new Position(4, 6)));
+        assertTrue(positions.contains(new Position(1, 7)));
+        assertTrue(positions.contains(new Position(7, 1)));
+        // check invalid positions
+        assertFalse(positions.contains(new Position(3, 5)));
+        assertFalse(positions.contains(new Position(-1, 5)));
+        assertFalse(positions.contains(new Position(3, 10)));
         assertEquals(positions.size(), 25);
+    }
+
+    @Test
+    @DisplayName("getAvailablePosition() (no board)")
+    void getAvailablePositionWithoutBoard() {
+        Piece queen = new Queen(new Player());
+
+        Set<Position> positions = queen.getAvailablePosition(false);
+        // check whether position is the same as expected
+        // check rank and tile
+        assertFalse(positions.contains(new Position(2, 5)));
+        assertEquals(positions.size(), 0);
+
     }
 
 }
