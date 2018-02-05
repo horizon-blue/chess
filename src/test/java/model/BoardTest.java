b61900fec6443ce0406c68559866bd8c67f8ae1b
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-02-05 14:30:48 -0600

    Add full coverage test

--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -109,22 +109,96 @@ class BoardTest {
 
     @Test
     void willBeChecked() {
+        Player whitePlayer = new Player();
+        Player blackPlayer = new Player();
+        whitePlayer.isWhite = true;
+        Board board = new Board();
+        board.addPiece(new King(whitePlayer), 4, 5);
+        board.addPiece(new King(blackPlayer), 4, 7);
+        board.addPiece(new Rook(whitePlayer), 1, 7);
+        Piece rook = new Rook(blackPlayer);
+        board.addPiece(rook, 3, 7);
+        assertFalse(board.willBeChecked(blackPlayer.king, 5, 7));
+        assertTrue(board.willBeChecked(rook, 3, 5));
+    }
+
+    @Test
+    @DisplayName("willBeChecked() (invalid position)")
+    void willBeCheckedInvalid() {
+        Player whitePlayer = new Player();
+        Player blackPlayer = new Player();
+        whitePlayer.isWhite = true;
+        Board board = new Board();
+        board.addPiece(new King(whitePlayer), 4, 5);
+        board.addPiece(new King(blackPlayer), 4, 7);
+        assertFalse(board.willBeChecked(whitePlayer.king, new Position(8, 8)));
     }
 
     @Test
     void isChecked() {
+        Player whitePlayer = new Player();
+        Player blackPlayer = new Player();
+        whitePlayer.isWhite = true;
+        Board board = new Board();
+        board.addPiece(new King(whitePlayer), 4, 5);
+        board.addPiece(new King(blackPlayer), 4, 7);
+        board.addPiece(new Rook(whitePlayer), 1, 7);
+        assertTrue(board.isChecked(blackPlayer));
     }
 
     @Test
-    void isCheckOrStaleMated() {
+    @DisplayName("isCheckOrStaleMated() (checkmate)")
+    void isCheckmated() {
+        Player whitePlayer = new Player();
+        Player blackPlayer = new Player();
+        whitePlayer.isWhite = true;
+        Board board = new Board();
+        board.addPiece(new King(whitePlayer), 4, 5);
+        board.addPiece(new King(blackPlayer), 4, 7);
+        board.addPiece(new Rook(whitePlayer), 1, 7);
+        assertTrue(board.isCheckOrStaleMated(blackPlayer));
+        assertTrue(board.isChecked(blackPlayer));
     }
+
+    @Test
+    @DisplayName("isCheckOrStaleMated() (stalemate)")
+    void isStalemated() {
+        Player whitePlayer = new Player();
+        Player blackPlayer = new Player();
+        whitePlayer.isWhite = true;
+        Board board = new Board();
+        board.addPiece(new King(whitePlayer), 6, 5);
+        board.addPiece(new King(blackPlayer), 7, 7);
+        board.addPiece(new Queen(whitePlayer), 5, 6);
+
+        assertTrue(board.isCheckOrStaleMated(blackPlayer));
+        assertFalse(board.isChecked(blackPlayer));
+    }
+
+    @Test
+    @DisplayName("isCheckOrStaleMated() (neither)")
+    void isNeitherCheckOrStaleMated() {
+        Player whitePlayer = new Player();
+        Player blackPlayer = new Player();
+        whitePlayer.isWhite = true;
+        Board board = new Board();
+        board.addPiece(new King(whitePlayer), 6, 5);
+        board.addPiece(new King(blackPlayer), 7, 7);
+
+        assertFalse(board.isCheckOrStaleMated(blackPlayer));
+        assertFalse(board.isChecked(blackPlayer));
+    }
+
 
     @Test
     @DisplayName("toString()")
     void toStringTest() {
         Board board = new Board();
         Player player = new Player();
-        board.addPiece(new Queen(player), 3, 5);
+        Piece queen = new Queen(player);
+        // highlighting won't affect block content
+        board.addPiece(queen, 3, 5);
+        System.out.println(board);
         String chessBoard = "8                \n" +
                 "7                \n" +
                 "6                \n" +
@@ -136,28 +210,30 @@ class BoardTest {
                 "  a b c d e f g h";
         assertEquals(chessBoard,
                 board.toString());
+
     }
 
     @Test
-    @DisplayName("toString() (with highlighting)")
-    void toStringTestWithHighlight() {
+    @DisplayName("toString() (colored)")
+    void toStringTestColored() {
         Board board = new Board();
         Player player = new Player();
         Piece queen = new Queen(player);
         // highlighting won't affect block content
         board.addPiece(queen, 3, 5);
         Set<Position> positions = queen.getAvailablePosition(false);
-        String chessBoard = "8                \n" +
-                "7                \n" +
-                "6                \n" +
-                "5                \n" +
-                "4           ♛    \n" +
-                "3                \n" +
-                "2                \n" +
-                "1                \n" +
+        // the ANSI codes are for coloring
+        String chessBoard = "8   \u001B[44m\u001B[1m \u001B[0m       \u001B[44m\u001B[1m \u001B[0m    \n" +
+                "7     \u001B[44m\u001B[1m \u001B[0m     \u001B[44m\u001B[1m \u001B[0m    \n" +
+                "6       \u001B[44m\u001B[1m \u001B[0m   \u001B[44m\u001B[1m \u001B[0m   \u001B[44m\u001B[1m \u001B[0m\n" +
+                "5         \u001B[44m\u001B[1m \u001B[0m \u001B[44m\u001B[1m \u001B[0m \u001B[44m\u001B[1m \u001B[0m  \n" +
+                "4 \u001B[44m\u001B[1m \u001B[0m \u001B[44m\u001B[1m \u001B[0m \u001B[44m\u001B[1m \u001B[0m \u001B[44m\u001B[1m \u001B[0m \u001B[44m\u001B[1m \u001B[0m ♛ \u001B[44m\u001B[1m \u001B[0m \u001B[44m\u001B[1m \u001B[0m\n" +
+                "3         \u001B[44m\u001B[1m \u001B[0m \u001B[44m\u001B[1m \u001B[0m \u001B[44m\u001B[1m \u001B[0m  \n" +
+                "2       \u001B[44m\u001B[1m \u001B[0m   \u001B[44m\u001B[1m \u001B[0m   \u001B[44m\u001B[1m \u001B[0m\n" +
+                "1     \u001B[44m\u001B[1m \u001B[0m     \u001B[44m\u001B[1m \u001B[0m    \n" +
                 "  a b c d e f g h";
         assertEquals(chessBoard,
-                board.toString());
+                board.toString(positions));
 
     }
 
